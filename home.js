4465d8d5c169916fce05829445e0a5587e6f2dee
John Richard <johnrichard20506@gmail.com>
2024-06-01 22:02:00 +0400

    new update

--- home.js
+++ home.js
@@ -1,5 +1,5 @@
 
-/*js code for the typing text effect*/ 
+/*js code for the typing text effect */ 
 document.addEventListener("DOMContentLoaded", function() {
     //const variable to declare the text var and its content
     const text = "\"A year 1 future Creative Computing graduate, who dreams of one day to succeed in all things, with God as my guide. Also an amateur producer that makes music that makes you go.\"";
